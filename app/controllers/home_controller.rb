0fb702d1a2a75fc61a973d496b582af70e023f43
jessicabarbalho <jessica@redu.com.br>
2013-09-15 23:31:54 -0300

    Método para recomendação, refatoração do controller de home, deixa view e método rousto caso não seja posível realizar a recomendação.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,44 +1,51 @@
 class HomeController < ApplicationController
-	
-	def index
-		if session.has_key? :user_id
-			@user = User.find_by_id(session[:user_id])
-			friends_list
-		end
-	end
+  def index
+    if session.has_key? :user_id
+      @user = User.find_by_id(session[:user_id])
+      friends_list
+    end
+  end
 
-	def friends_list
-		conn = Faraday.new(:url => 'https://graph.facebook.com/') do |faraday|
-			faraday.request  :url_encoded
-			faraday.response :logger
-			faraday.adapter  Faraday.default_adapter
-		end
+  def friends_list
+    @user = User.find_by_id(session[:user_id])
+    friends_response = get_friends(@user)
 
-		@user = User.find_by_id(session[:user_id])
+    @friends = JSON.parse(friends_response.body)['friends']['data']
 
-		friends_response = conn.get @user.uid + "?fields=friends.fields(id,name,picture)&access_token=" + @user.access_token
+    render :action => 'friends_list'
+  end
 
-		require 'json'
-		@friends = JSON.parse(friends_response.body)['friends']['data']
+  def get_friend_likes
+    @user = User.find_by_id(session[:user_id])
+    likes_response = get_likes(params[:friend_uid], @user)
 
-		render :action => 'friends_list'
-	end
+    unless likes_response.body['music'].nil?
+      @friend_likes = JSON.parse(likes_response.body)['music']['data']
+      @friend_recomendation = Band.get_recommendation(@friend_likes).try(:shuffle!)
+    end
 
-	def get_friend_likes
-		conn = Faraday.new(:url => 'https://graph.facebook.com/') do |faraday|
-			faraday.request  :url_encoded
-			faraday.response :logger
-			faraday.adapter  Faraday.default_adapter
-		end
+    @friend_name = JSON.parse(likes_response.body)['name']
 
-		@user = User.find_by_id(session[:user_id])
-		likes_response = conn.get params[:friend_uid] + "?fields=name,music&access_token=" + @user.access_token
+    render :action => 'gifting_friend'
+  end
 
-		require 'json'
-		@friend_likes = JSON.parse(likes_response.body)['music']['data']
-		@friend_name = JSON.parse(likes_response.body)['name']
+  private
 
-		render :action => 'gifting_friend'
-	end
+  def get_friends(user)
+    conn = create_connection
+    conn.get user.uid + "?fields=friends.fields(id,name,picture)&access_token=" + user.access_token
+  end
 
+  def get_likes(friend_id, user)
+    conn = create_connection
+    likes_response = conn.get friend_id + "?fields=name,music&access_token=" + user.access_token
+  end
+
+  def create_connection
+    conn = Faraday.new(:url => 'https://graph.facebook.com/') do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
 end
--- app/models/band.rb
+++ app/models/band.rb
@@ -3,4 +3,10 @@ class Band < ActiveRecord::Base
   has_many :cds, dependent: :destroy
 
   validates :name, presence: :true, uniqueness: :true
+
+  def self.get_recommendation(artists)
+    bands = Band.where(name: artists.map{ |a| a["name"]})
+    similars = bands.try { |b| b.map { |band| band.bands }.flatten! }
+    similars.try { |s| s.map { |similar| similar.cds }.flatten! }
+  end
 end
